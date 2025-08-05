78492cbeb437f090e5be8fbc963f9d682fb2cc9b
Abhishek <abhisarkar0805@gmail.com>
2025-08-05 21:11:30 +0530

    update

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var todoItems = document.querySelectorAll('.todo')
-var fullElem = document.querySelectorAll('.fullElem')
-var allElem = document.querySelector('.allElem') // Main grid section
-var bac = document.querySelectorAll('.fullElem .back')
+function openCards(){
+    var todoItems = document.querySelectorAll('.todo')
+    var fullElem = document.querySelectorAll('.fullElem')
+    var allElem = document.querySelector('.allElem') // Main grid section
+    var bac = document.querySelectorAll('.fullElem .back')
 
-todoItems.forEach(function(elem){
-    elem.addEventListener('click', function(){
+    todoItems.forEach(function(elem){
+        elem.addEventListener('click', function(){
         // Main grid hide karo
-        allElem.style.display = 'none'
+            allElem.style.display = 'none'
         
         // Pehle saare fullElem hide karo
-        fullElem.forEach(function(section){
-            section.style.display = 'none'
-        })
+            fullElem.forEach(function(section){
+                section.style.display = 'none'
+            })
         
         // Selected fullElem show karo
-        fullElem[parseInt(elem.id)].style.display = 'block'
+            fullElem[parseInt(elem.id)].style.display = 'block'
+        })
     })
-})
-bac.forEach(function(back){
-    back.addEventListener('click', function(){
-        fullElem[back.id].style.display='none'
-        allElem.style.display = 'grid'
+    bac.forEach(function(back){
+        back.addEventListener('click', function(){
+            fullElem[back.id].style.display='none'
+            allElem.style.display = 'grid'
+        })
     })
-})
+}
+openCards()
 
